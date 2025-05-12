d62e10fafa6ad971fb3940624f10013c4f4d21fa
Riky1 <cresti.ric@gmail.com>
2025-05-12 10:17:26 +0200

    chore: add swiper

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,69 @@
 <script setup>
 import { ref, onMounted, watch } from 'vue';
+import { register } from 'swiper/element/bundle';
 import { useFetchData } from './composables/useFetchData';
 import VideoPlayer from './components/VideoPlayer.vue';
 
+register();
+
 const { jsonData, loading, error, loadConfig } = useFetchData();
 
+const swiperRef = ref(null);
+const isVisible = ref(false);
 const videosData = ref([]);
 const globalOptions = ref({});
 
 onMounted(() => {
   loadConfig();
+
+  // per personalizzare gli stili del componente Swiper
+  setTimeout(() => {
+    const swiperEl = swiperRef.value;
+
+    if (swiperEl) {
+      const params = {
+        injectStyles: [
+          `
+            .swiper {
+              width: calc(100% - 40px);
+              padding: 0 20px;
+            }
+              
+            .swiper-button-next,
+            .swiper-button-prev {
+              height: 24px;
+            }
+
+            .swiper-button-prev {
+              left: 0;
+            }
+
+            .swiper-button-next {
+              right: 0;
+            }
+
+            .swiper-pagination-bullet{
+              width: 10px;
+              height: 10px;
+              margin: 0 3px;
+              border: 2px solid #ddd;
+              border-radius: 50%;
+              background-color: #949494;
+              opacity: 1;
+            }
+            .swiper-pagination-bullet-active {
+              background-color: #295eb9;
+              opacity: 1;
+            }
+          `,
+        ],
+      };
+
+      Object.assign(swiperEl, params);
+      swiperEl.initialize();
+      isVisible.value = true;
+    }
+  }, 1000);
 });
 
 watch(jsonData, (newValue) => {
@@ -32,22 +86,41 @@ console.log('jsonData: ', jsonData);
 // console.log('lang: ', lang);
 </script>
 
-<template>
+<template v-show="isVisible">
   <main>
     <div v-if="loading">Loading…</div>
     <div v-else-if="error">Error: {{ error.message }}</div>
     <template v-else>
-      <div v-for="(video, index) in videosData" :key="index" class="video-wrapper">
-        <VideoPlayer 
-          :sources="video.sources"
-          :poster="video.poster" 
-          :description="video.posterAlt" 
-          :aspectRatio="video.aspectRatio"
-          :subtitles="video.subtitles"
-          :options="video.options"
-          :globalOptions="globalOptions"
-        />
-      </div>
+      <swiper-container
+        ref="swiperRef"
+        class="swiper-container"
+        init="false"
+        :slidesPerView="3"
+        :spaceBetween="0"
+        :navigation="true"
+        :pagination="{
+          clickable: true,
+        }"
+        :keyboard="{
+          enabled: true,
+        }"
+      >
+        <swiper-slide
+          v-for="(video, index) in videosData" :key="index" 
+        >
+          <div class="video-wrapper">
+            <VideoPlayer 
+              :sources="video.sources"
+              :poster="video.poster" 
+              :description="video.posterAlt" 
+              :aspectRatio="video.aspectRatio"
+              :subtitles="video.subtitles"
+              :options="video.options"
+              :globalOptions="globalOptions"
+            />
+          </div>
+        </swiper-slide>
+      </swiper-container>
     </template>
   </main>
 </template>
@@ -57,18 +130,22 @@ main {
   display: flex;
   justify-content: center;
   align-items: center;  
+  width: 100%;
+}
+
+.swiper-container {
+  width: 100%;
+  height: 100%;
 }
 
 .video-wrapper {
-  position: relative;
-  width: 100%;
-  max-width: 300px;
-  border: 3px solid red;
-  border-radius: 30px;
+  border-radius: 16px;
   overflow: hidden;
   transition: transform 0.3s ease-in-out;
   transform-origin: center;
-  margin: 40px;
+  margin: 40px 20px 50px;
+  margin-bottom: 50px;
+  margin-top: 40px;
 
   &:hover {
     transform: scale(1.1);
